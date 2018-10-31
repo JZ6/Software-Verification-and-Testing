9a51641bccb40ba134beb12d0390ff2759b6e3df
JZ6 <yjzuo6@gmail.com>
2018-10-31 17:56:08 -0400

    add jpf generated test cases

---
+++ A2/src/test/java/nl/tudelft/jpacman/board/JPFBoardTest.java
@@ -0,0 +1,43 @@
+package nl.tudelft.jpacman.board;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+class JPFBoardTest {
+
+    private static final int MAX_WIDTH = 2;
+    private static final int MAX_HEIGHT = 3;
+
+    private final Square[][] grid = { { mock(Square.class), mock(Square.class), mock(Square.class) },
+            { mock(Square.class), mock(Square.class), mock(Square.class) }, };
+    private final Board board = new Board(grid);
+   
+    @Test
+    void test0() {
+        assertThat(board.withinBorders(0, 0)).isEqualTo(true);
+    }
+
+    @Test
+    public void test1() {
+        assertThat(board.withinBorders(0, 3)).isEqualTo(false);
+    }
+
+    @Test
+    public void test2() {
+        assertThat(board.withinBorders(0, -10)).isEqualTo(false);
+    }
+
+    @Test
+    public void test3() {
+        assertThat(board.withinBorders(2, -10)).isEqualTo(false);
+    }
+
+    @Test
+    public void test4() {
+        assertThat(board.withinBorders(-10, -10)).isEqualTo(false);
+    }
+}
