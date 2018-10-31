2779fb6d37329f2633df526d06064664c650dff2
basictroll <shens494@hotmail.com>
2018-10-31 01:02:39 -0400

    -added JPF tests for SquareFree.java

---
+++ A2/src/test/java/nl/tudelft/jpacman/squareFree/nl_tudelft_jpacman_squareFree_SquareFreeTest.java
@@ -0,0 +1,263 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class nl_tudelft_jpacman_squareFree_SquareFreeTest {
+
+    private nl.tudelft.jpacman.squareFree.SquareFree nl_tudelft_jpacman_squarefree_squarefree;
+
+    @Before
+    public void setUp() throws Exception {
+        nl_tudelft_jpacman_squarefree_squarefree = new nl.tudelft.jpacman.squareFree.SquareFree();
+    }
+
+    @Test
+    public void test0() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(-50);
+    }
+
+    @Test
+    public void test1() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(4);
+    }
+
+    @Test
+    public void test2() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(8);
+    }
+
+    @Test
+    public void test3() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(12);
+    }
+
+    @Test
+    public void test4() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(16);
+    }
+
+    @Test
+    public void test5() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(20);
+    }
+
+    @Test
+    public void test6() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(24);
+    }
+
+    @Test
+    public void test7() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(28);
+    }
+
+    @Test
+    public void test8() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(32);
+    }
+
+    @Test
+    public void test9() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(36);
+    }
+
+    @Test
+    public void test10() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(40);
+    }
+
+    @Test
+    public void test11() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(44);
+    }
+
+    @Test
+    public void test12() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(48);
+    }
+
+    @Test
+    public void test13() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(50);
+    }
+
+    @Test
+    public void test14() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(49);
+    }
+
+    @Test
+    public void test15() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(45);
+    }
+
+    @Test
+    public void test16() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(47);
+    }
+
+    @Test
+    public void test17() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(46);
+    }
+
+    @Test
+    public void test18() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(43);
+    }
+
+    @Test
+    public void test19() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(42);
+    }
+
+    @Test
+    public void test20() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(41);
+    }
+
+    @Test
+    public void test21() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(39);
+    }
+
+    @Test
+    public void test22() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(38);
+    }
+
+    @Test
+    public void test23() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(37);
+    }
+
+    @Test
+    public void test24() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(35);
+    }
+
+    @Test
+    public void test25() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(34);
+    }
+
+    @Test
+    public void test26() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(33);
+    }
+
+    @Test
+    public void test27() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(31);
+    }
+
+    @Test
+    public void test28() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(30);
+    }
+
+    @Test
+    public void test29() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(29);
+    }
+
+    @Test
+    public void test30() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(27);
+    }
+
+    @Test
+    public void test31() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(25);
+    }
+
+    @Test
+    public void test32() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(26);
+    }
+
+    @Test
+    public void test33() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(23);
+    }
+
+    @Test
+    public void test34() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(22);
+    }
+
+    @Test
+    public void test35() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(21);
+    }
+
+    @Test
+    public void test36() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(18);
+    }
+
+    @Test
+    public void test37() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(19);
+    }
+
+    @Test
+    public void test38() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(17);
+    }
+
+    @Test
+    public void test39() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(15);
+    }
+
+    @Test
+    public void test40() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(14);
+    }
+
+    @Test
+    public void test41() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(13);
+    }
+
+    @Test
+    public void test42() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(9);
+    }
+
+    @Test
+    public void test43() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(11);
+    }
+
+    @Test
+    public void test44() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(10);
+    }
+
+    @Test
+    public void test45() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(7);
+    }
+
+    @Test
+    public void test46() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(6);
+    }
+
+    @Test
+    public void test47() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(5);
+    }
+
+    @Test
+    public void test48() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(3);
+    }
+
+    @Test
+    public void test49() {
+        nl_tudelft_jpacman_squarefree_squarefree.isSquareFree(1);
+    }
+}
