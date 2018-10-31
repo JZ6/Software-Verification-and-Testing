edc0f78d5bf9c11f393b1ee8497d6a04ea8bad59
JZ6 <yjzuo6@gmail.com>
2018-10-31 18:14:19 -0400

    Robust worst-case boundary-value test cases

--- A2/src/test/java/nl/tudelft/jpacman/board/JPFBoardTest.java
+++ A2/src/test/java/nl/tudelft/jpacman/board/JPFBoardTest.java
@@ -15,7 +15,7 @@ class JPFBoardTest {
     private final Square[][] grid = { { mock(Square.class), mock(Square.class), mock(Square.class) },
             { mock(Square.class), mock(Square.class), mock(Square.class) }, };
     private final Board board = new Board(grid);
-   
+
     @Test
     void test0() {
         assertThat(board.withinBorders(0, 0)).isEqualTo(true);
@@ -39,5 +39,30 @@ class JPFBoardTest {
     @Test
     public void test4() {
         assertThat(board.withinBorders(-10, -10)).isEqualTo(false);
+    }
+
+    @Test
+    public void test5() {
+        assertThat(board.withinBorders(1, 1)).isEqualTo(true);
+    }
+
+    @Test
+    public void test6() {
+        assertThat(board.withinBorders(2, 0)).isEqualTo(false);
+    }
+
+    @Test
+    public void test7() {
+        assertThat(board.withinBorders(-10, 3)).isEqualTo(false);
+    }
+
+    @Test
+    public void test8() {
+        assertThat(board.withinBorders(-10, 0)).isEqualTo(false);
+    }
+
+    @Test
+    public void test9() {
+        assertThat(board.withinBorders(2, 3)).isEqualTo(false);
     }
 }
